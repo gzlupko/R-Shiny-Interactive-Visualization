46853f008a7506911bfdbb849c3940662075b73d
gzlupko <gianzlupko@gmail.com>
2021-02-20 16:41:48 -0500

    shiny dashboard layout shell added

--- interactive_student_test_data.R
+++ interactive_student_test_data.R
@@ -1,20 +1,29 @@
 
 
 
-
-
 # libraries 
 
 library(shiny)
+library(dplyr) 
+library(ggplot2) 
+library(readr) 
+library(shinydashboard) 
+
+# load student quiz categories and midterm test results 
+
+midterm <- read_csv("midterm-results.csv") 
+quiz_cat <- read_csv("quiz-categories.csv") 
 
 
 
 
 # UI
-ui <- fluidPage() 
-
-
-
+ui <- dashboardPage( 
+    dashboardHeader(), 
+    dashboardSidebar(), 
+    dashboardBody()
+    
+    )
 
 
 
