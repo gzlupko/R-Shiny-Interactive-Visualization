e2967707b96eaea5b7f6ee785428ba616750a90f
gzlupko <gianzlupko@gmail.com>
2021-02-20 16:20:56 -0500

    initial commit for app code

---
+++ interactive_student_test_data/interactive_student_test_data.R
@@ -0,0 +1,32 @@
+
+
+
+
+
+# libraries 
+
+library(shiny)
+
+
+
+
+# UI
+ui <- fluidPage() 
+
+
+
+
+
+
+
+
+# Server 
+server <- function(input, output) {} 
+
+
+
+
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
