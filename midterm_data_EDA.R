ee214885e4b5ba4d19ab61fd4f90bd0f53937fc5
gzlupko <gianzlupko@gmail.com>
2021-02-22 08:18:16 -0500

    filtering question time to use for question difficulty calculation

--- midterm_data_EDA.R
+++ midterm_data_EDA.R
@@ -8,6 +8,7 @@ quiz_cat <- read_csv("quiz-categories.csv")
 
 
 
+View(quiz_cat) 
 
 
 
@@ -26,25 +27,105 @@ question_difficulty <- midterm %>%
 
 question_difficulty <- function(x) { 
   
-  x <- paste(x, "_c", sep = "") 
-  y <- paste(x, "_time", sep = "") 
+  question_number <- as.character(paste("Q", x, "_c", sep =""))  
+  question_time <- as.character(paste("Q", x, "_time", sep = "")) 
   
   midterm %>%
-    select(x, y) %>%
-    mutate(question_difficulty = mean(x) * mean(y)) 
+    summarize(pase = mean(question_number) * mean(question_time)) 
   }
 
 
 
-question_difficulty(midterm$Q1) 
+question_difficulty(1) 
 
 
 
-quiz_cat %>%
-  group_by(wrangling) 
+midterm_filtered <- midterm %>%
+  select(Q1_c, Q1_time, Q2_c, Q2_time, Q3_c, Q3_time, Q4_c, Q4_time, 
+         Q5_c, Q5_time, Q6_c, Q6_time, Q7_c, )
 
 
+
+# brute force approach ad
+difficulty_column <- midterm %>%
+  summarize(Q1_difficulty = mean(Q1_c) * mean(Q1_time), 
+            Q2_difficulty = mean(Q2_c) * mean(Q2_time), 
+            Q3_difficulty = mean(Q3_c) * mean(Q3_time), 
+            Q4_difficulty = mean(Q4_c) * mean(Q4_time), 
+            Q5_difficulty = mean(Q5_c) * mean(Q5_time), 
+            Q6_difficulty = mean(Q6_c) * mean(Q6_time), 
+            Q7_difficulty = mean(Q7_c) * mean(Q7_time), 
+            Q8_difficulty = mean(Q8_c) * mean(Q8.9_time), 
+            Q10_difficulty = mean(Q10_c) * mean(Q10_time), 
+            Q11_difficulty = mean(Q11_c) * mean(Q11_time), 
+            Q12_ifficulty = mean(Q12_c) * mean(Q12_time), 
+            Q13_difficulty = mean(Q13_c) * mean(Q13_time), 
+            Q14_difficulty = mean(Q14_c) * mean(Q14_time), 
+            Q15_difficulty = mean(Q15_c) * mean(Q15_time), 
+            Q16_difficulty = mean(Q16_c) * mean(Q16_time), 
+            Q17_difficulty = mean(Q17_c) * mean(Q17_time))  
+
+
+topics_and_difficulty <- cbind(quiz_cat, difficulty_column) 
+
+
+difficulty_calculation <- function(x) { 
   
+  question_number <- paste("Q", x, "_c", sep ="") 
+  question_time <- paste("Q", x, "_time") 
+  
+  midterm %>%
+summarize(Qx_difficulty = mean(question_number)*mean(question_time)) } 
+
+
+
+
+test_output <- function(x) { 
+  
+  question_number <- as.character(paste("Q", x, "_c", sep ="")) 
+  paste(question_number)
+} 
+
+
+
+
+question_difficulty <- function(x) { 
+  
+  question_number <- paste("Q", x, "_c", sep ="", collapse = "")
+  mean(midterm$question_number) 
+  
+} 
+
+
+
+question_difficulty(3) 
+
+
+
+paste("Q", 3, "_c", sep ="", collapse = "")
+
+
+
+as.character(paste("Q", 3, "_c", sep = "")) 
+
+class(midterm$Q10_c) 
+
+
+
+
+test_calculation <- difficulty_calculation(3)          
+View(test_calculation) 
+
+
+View(difficulty_calculation(Q3))           
+
+
+
+      
+
+
+
+
 
 
   
