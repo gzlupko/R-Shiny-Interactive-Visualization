d8e54afa56b3720d15fb4c94302bf515d4382cd5
gzlupko <gianzlupko@gmail.com>
2021-02-23 20:35:52 -0500

    app success

--- interactive_student_test_data.R
+++ interactive_student_test_data.R
@@ -14,7 +14,7 @@ library(shinydashboard)
 midterm <- read_csv("midterm-results.csv") 
 quiz_cat <- read_csv("quiz-categories.csv") 
 
-midterm_filter <- midterm %>%
+#midterm_filter <- midterm %>%
     rename(Q1 = Q1_c, Q2 = Q2_c, Q3 = Q3_c, Q4 = Q4_c)
 
 
@@ -44,10 +44,15 @@ ui <- dashboardPage(
 server <- function(input, output) {
     
     output$quiz_help <- renderPlot({ 
-        quiz_data <- quiz_cat %>%
+        
+      search_count <- quiz_cat %>%
+         count(input$group) 
+      
+      quiz_data <- quiz_cat %>%
             group_by(googleable, non.googleable) 
         
-        ggplot(data = quiz_data, aes(x = input$group)) + geom_bar(stat = "identity")
+        ggplot(data = quiz_data, aes(x = input$group, y = search_count) + 
+                 geom_bar(stat = "identity"))
         
         })
         
@@ -61,7 +66,7 @@ shinyApp(ui = ui, server = server)
 
 
 
-# second app 
+# second app using reactive 
 
 
 # UI
@@ -121,6 +126,18 @@ shinyApp(ui = ui, server = server)
 
 # UI
 
+
+
+
+
+midterm1 <- midterm %>%
+  select(id, Q1_time, Q2_time, Q3_time, Q4_time, Q5_time, Q6_time, 
+         Q7_time) 
+
+midterm1
+colnames(midterm1) 
+choices <- colnames(midterm1) 
+
 ui <- dashboardPage( 
     dashboardHeader(
         title = "Visualizing Student Test Data"
@@ -129,38 +146,85 @@ ui <- dashboardPage(
     dashboardBody(
         
         box(
-            selectInput(inputId = "Question", label = "Question Number: ", 
-                        choices = c("Q1", "Q2", "Q3", "Q4", "Q5", 
-                                    "Q6", "Q7")), 
+            varSelectInput(inputId = "question", 
+                           label = "Question Duration: ", midterm1),  
             plotOutput('question_time')
         )
     )
     
+) 
+
+
+# Server 
+
+server <- function(input, output) { 
+    
+  
+  
+        output$question_time <- renderPlot({
+          
+                ggplot(midterm1, aes(x = id, y = !!input$question)) + 
+                           geom_bar(stat = "identity") + 
+            xlab("Students") + ylab("Question Duration (seconds)") 
+        })
+    
+} 
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+# old version with concatenate and question_number and question_duration
+
+
+# UI
+
+ui <- dashboardPage( 
+  dashboardHeader(
+    title = "Visualizing Student Test Data"
+  ), 
+  dashboardSidebar(), 
+  dashboardBody(
+    
+    box(
+      selectInput(inputId = "Question", label = "Question Number: ", 
+                  choices = c("Q1_time", "Q2_time", "Q3_time", 
+                              "Q4_time", "Q5_time", 
+                              "Q6_time", "Q7_time")), 
+      plotOutput('question_time')
+    )
+  )
+  
 )
 
 
 # Server 
 
 server <- function(input, output) {
+  
+  
+  
+  output$question_time <- renderPlot({ 
     
-
-    output$question_time <- renderPlot({ 
-        
-    question_duration <- as.numeric(paste(input$Question, "_time", sep = "")) 
-     question_number <- as.numeric(paste(input$Question, "_c", sep = ""))
-                                        
-        output$question_time <- renderPlot({ 
-            
-            students <- as.factor(midterm$id)
-            
-            midterm_filter %>%
-                group_by(question_number) %>%
-                ggplot(aes_string(x = students, y = question_duration) + 
-                           geom_bar(stat = "identity"))
-      
-    })
+    midterm <- read_csv("midterm-results.csv")
     
-}) 
+    
+    question_duration <- paste(input$Question, "_time", sep = "")
+    question_number <- paste(input$Question, "_c", sep = "")
+    students <- as.factor(midterm$id)
+    
+    midterm %>%
+      ggplot(aes(x = students, y = question_duration)) + 
+      geom_bar(stat = "identity")
+    
+  })
+  
 }
 
 
@@ -171,7 +235,76 @@ shinyApp(ui = ui, server = server)
 
 
 
-# 4th attempt 
+# test new by using function to define new objects 
+
+
+# UI
+
+ui <- dashboardPage( 
+  dashboardHeader(
+    title = "Visualizing Student Test Data"
+  ), 
+  dashboardSidebar(), 
+  dashboardBody(
+    
+    box(
+      selectInput(inputId = "Question", label = "Question Number: ", 
+                  choices = c("Q1", "Q2", "Q3", "Q4", "Q5", 
+                              "Q6", "Q7")), 
+      plotOutput('question_time') 
+    )
+  )
+  
+)
+
+
+# Server 
+
+server <- function(input, output) {
+  
+  
+  output$mytable <- renderDataTable({
+    
+    midterm <- read_csv("midterm-results.csv") }) 
+  
+  
+  
+  output$question_time <- renderPlot({ 
+  
+    
+    duration <- paste(input$Question, "_time", sep = "")  
+    
+    students <- (midterm$id) 
+    
+    question_duration <- midterm[ , duration]
+    
+    new_table$students <- students
+    new_table$question_number <- input$Question
+    new_table$question_duration <- question_duration
+    new_table <- as.data.frame(new_table) 
+    
+    
+    new_table %>%
+      ggplot(aes(x = students, y = question_duration)) + 
+      geom_bar(stat = "identity")
+}) 
+    
+} 
+
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+# 3rd attempt with observeEvent 
 
 # UI
 
